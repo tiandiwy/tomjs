28716f4ac89c0a7e3c3818a034ebf06613a4d30e
tiandiwy <tiandiwy@aliyun.com>
2019-07-10 21:31:57 +0800

    修复 init_dir判断 FRAMEWORK_DEVELOPMENT 的bug

--- handlers/init_dir.js
+++ handlers/init_dir.js
@@ -2,6 +2,6 @@
 require('universal-dotenv');
 const path = require('path');
 const AppDir = require('tomjs/handlers/dir');
-let appdir = path.join(__dirname, process.env.FRAMEWORK_DEVELOPMENT ? '../../..' : '../../../src');
+let appdir = path.join(__dirname, process.env.FRAMEWORK_DEVELOPMENT == "true" ? '../../..' : '../../../src');
 AppDir(appdir);
 module.exports = appdir;
