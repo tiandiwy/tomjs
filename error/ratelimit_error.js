4947b53d0c7027d09f2a965bf0c4a4801d6f5932
tiandiwy <tiandiwy@aliyun.com>
2019-08-14 15:20:23 +0800

    删除无用的ratelimit_error.js文件

--- error/ratelimit_error.js
+++
@@ -1,9 +0,0 @@
-class RatelimitError extends Error {
-    constructor(message, info) {
-        super(message);
-        this.info = info;
-        this.name = "RatelimitError";
-    }
-}
-
-module.exports = RatelimitError;
