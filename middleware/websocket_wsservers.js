22f77ee9b427d5d0da8096be62307352611d6ea6
tiandiwy <tiandiwy@aliyun.com>
2019-09-05 10:12:23 +0800

    去掉无用的代码

--- middleware/websocket_wsservers.js
+++ middleware/websocket_wsservers.js
@@ -4,6 +4,6 @@ const AllWsServers = require2('tomjs/handlers/all_ws_server').getAllWS();
 module.exports = function (ctx, next) {
     ctx.websocket.servers = AllWsServers;
     //bind room相关函数
-    AllWsServers.ctxBindBroadcastFn(ctx);
+    //AllWsServers.ctxBindBroadcastFn(ctx);
     return next();
 };
