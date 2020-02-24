6a9cae5fd0f8051525e897bd9d14413b86ec19ed
tiandiwy <tiandiwy@aliyun.com>
2020-02-24 20:54:24 +0800

    getDBObjByID的pql参数支持ctx中自动获取

--- handlers/db_tools.js
+++ handlers/db_tools.js
@@ -1,21 +1,35 @@
 const require2 = require('tomjs/handlers/require2');
 const BaseApiError = require2('tomjs/error/base_api_error');
+const { isObject } = require2('tomjs/handlers/base_tools');
+const models_cfg = require2('tomjs/configs')().models;
 exports.getDBObjByID = async (mode, id, pql = undefined) => {
     let obj = undefined;
     try {
         if (pql === undefined) { obj = await mode.findById(id); }
         else {
+            if (isObject(pql)) {
+                if (pql.app.constructor.name == "Application") {
+                    try {
+                        req = Object.assign({}, pql.request.query ? pql.request.query : {}, pql.request.body ? pql.request.body : {});
+                        if (req[models_cfg.pql.ctx_body_query_field]) {
+                            pql = req[models_cfg.pql.ctx_body_query_field];
+                        }
+                        else {
+                            pql = {};
+                        }
+                    } catch (error) { pql = {}; }
+                }
+            }
             obj = await mode.findById(id).pql(pql);
         }
-
     } catch (error) {
         if (error instanceof BaseApiError) {
             throw error;
         }
-        else{
+        else {
             throw new BaseApiError(BaseApiError.DB_ERROR, { id: id, message: error.message });
         }
-        
+
     }
     if (obj == null) {
         throw new BaseApiError(BaseApiError.DB_NOT_FOUND_ERROR, { id: id });
