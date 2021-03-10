dfbb80a4a6a05918113d6d4300cd0f18f23cbce2
tiandiwy <tiandiwy@aliyun.com>
2021-03-10 09:56:23 +0800

    对cache支持多配置

--- cache/index.js
+++ cache/index.js
@@ -1,19 +1,23 @@
 const require2 = require('tomjs/handlers/require2');
 const config = require2('tomjs/configs')().cache;
 const Cacheman = require2('cacheman')
-let default_cache = new Cacheman('default', config);
+let default_cache = new Cacheman('default', config.default ? config.default : config);
 let all_caches = {};
 
 function cache(name, options) {
     if (name === undefined) {
         return default_cache;
     }
-    if(all_caches[name])
-    {
+    if (all_caches[name]) {
         return all_caches[name];
     }
-    else{
-        all_caches[name] = new Cacheman(name, options);
+    else {
+        if (options) {
+            all_caches[name] = new Cacheman(name, options);
+        }
+        else {
+            all_caches[name] = new Cacheman(name, config.name);
+        }
         return all_caches[name];
     }
 }
