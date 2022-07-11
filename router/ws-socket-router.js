080764acef07bfa0f9cd1a44bdd2bf818673b31b
tiandiwy <tiandiwy@aliyun.com>
2022-07-11 22:38:47 +0800

    设置WSSocketRouter默认不需要再身份验证了

--- router/ws-socket-router.js
+++ router/ws-socket-router.js
@@ -4,6 +4,7 @@ const LaravelRouter = require2('tomjs/router/main-router');
 class WSSocketRouter extends LaravelRouter {
     constructor() {
         super('./websocket');
+        this.auth(false, false);
     }
 }
 module.exports = WSSocketRouter;
